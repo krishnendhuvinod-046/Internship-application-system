c07c15fca89d4d2930bb154c8da40fb2a0d390dc
krishnendhuvinod-046 <krishnendhuvinod046@gmail.com>
2026-03-04 10:38:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import pandas as pd
-
-# Read the CSV file
 df = pd.read_csv("applications.csv")
-
-# -----------------------------
-# 1️⃣ Shortlist Candidates
-# -----------------------------
 
 shortlisted = df[
     (df["CGPA"] >= 8.0) &
@@ -18,22 +12,12 @@ print(shortlisted)
 
 shortlisted.to_csv("shortlisted.csv", index=False)
 
-
-# -----------------------------
-# 2️⃣ Incomplete Applications
-# -----------------------------
-
 incomplete = df[df.isnull().any(axis=1)]
 
 print("\nIncomplete Applications:")
 print(incomplete)
 
 incomplete.to_csv("incomplete.csv", index=False)
-
-
-# -----------------------------
-# 3️⃣ Duplicate Applications
-# -----------------------------
 
 duplicates = df[df.duplicated(subset="Email", keep=False)]
 
@@ -44,3 +28,4 @@ duplicates.to_csv("duplicates.csv", index=False)
 
 
 print("\nAll files generated successfully ✅")
+
